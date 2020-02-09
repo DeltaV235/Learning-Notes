bd8e529dd4bb656b227d4a2911798b99464d26d8
DeltaV235 <wuyue96@gmail.com>
2020-02-09 22:47:29 +0800

    docs(css & js): 完成了简单了CSS练习和变量相关JS练习

---
+++ html-learning/train/Js_Train/demo02.js
@@ -0,0 +1,35 @@
+/*
+    定义变量
+ */
+
+// var a = 3;
+//
+// alert(a);
+
+var num1 = 1;
+var num2 = 4.6;
+var num3 = NaN;
+
+document.write(num1 + "---" + typeof num1 + "<br>")
+document.write(num2 + "---" + typeof num2 + "<br>")
+document.write(num3 + "---" + typeof num3 + "<br>")
+
+var str1 = 'abc';
+var str2 = "def";
+
+document.write(str1 + "---" + typeof str1 + "<br>")
+document.write(str2 + "---" + typeof str2 + "<br>")
+
+var flag = false;
+
+document.write(flag + "---" + typeof flag + "<br>")
+
+var obj1 = null;
+var obj2 = undefined;
+var obj3;
+document.write(obj1 + "---" + typeof obj1 + "<br>")
+document.write(obj2 + "---" + typeof obj2 + "<br>")
+document.write(obj3 + "---" + typeof obj3 + "<br>")
+
+
+
