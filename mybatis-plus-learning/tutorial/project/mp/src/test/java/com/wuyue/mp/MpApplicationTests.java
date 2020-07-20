bccfbfc37fb640fdd937dd9ecef6fd62e351f5ae
DeltaV235 <wuyue96@gmail.com>
2020-07-21 00:46:25 +0800

    tutorial(MP): Add tutorial project for Mybatis Plus.

---
+++ mybatis-plus-learning/tutorial/project/mp/src/test/java/com/wuyue/mp/MpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuyue.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
