37aeabfb570b2fabd3e3bf1033491e53578d5c37
DeltaV235 <wuyue96@gmail.com>
2020-07-25 12:12:18 +0800

    fix(mp): 修改了MapperScan的value值，测试类增加了Runwith注解

--- mybatis-plus-learning/tutorial/project/mp/src/main/java/com/wuyue/mp/MpApplication.java
+++ mybatis-plus-learning/tutorial/project/mp/src/main/java/com/wuyue/mp/MpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("**.mapper.*")
+@MapperScan("com.wuyue.**.mapper")
 public class MpApplication {
 
     public static void main(String[] args) {
--- mybatis-plus-learning/tutorial/project/mp/src/test/java/com/wuyue/mp/MpApplicationTests.java
+++ mybatis-plus-learning/tutorial/project/mp/src/test/java/com/wuyue/mp/MpApplicationTests.java
@@ -3,20 +3,19 @@ package com.wuyue.mp;
 import com.wuyue.mp.entities.User;
 import com.wuyue.mp.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class MpApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
-
-    @Test
-    void contextLoads() {
-    }
 
     @Test
     public void select() {
