fa50c631385c38e43cc31398b1946512babf55fe
DeltaV235 <wuyue96@gmail.com>
2020-11-08 02:40:18 +0800

    feat(idea-setting-test-demo): Add JavaDocTest demo.

---
+++ 03-tools/idea/demo-project/idea-setting-test-demo/javadoc/src/com/wuyue/JavaDocTest.java
@@ -0,0 +1,32 @@
+package com.wuyue;
+
+/**
+ * The type Java doc test.
+ *
+ * @author DeltaV235
+ * @version 1.0
+ * @date 2020 /11/8 2:32
+ */
+public class JavaDocTest {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Foo int.
+     *
+     * @param param  param1
+     * @param param2 the param 2
+     * @return int int
+     * @author wuyue
+     * @date 2020 /11/8 2:37
+     */
+    public int foo(int param, String param2) {
+        return 1;
+    }
+}
