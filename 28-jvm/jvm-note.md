244bbe8a854fbffc3e38f7fd457ebb8846b7e074
DeltaV235 <wuyue96@gmail.com>
2021-02-27 21:14:53 +0800

    feat(jvm): 增加了类加载双亲委派机制的测试类。

--- 28-jvm/jvm-note.md
+++ 28-jvm/jvm-note.md
@@ -246,7 +246,6 @@ TODO 详细图
 public class HelloApp {
     private static int a = 1;//prepare：a = 0 ---> initial : a = 1
 
-
     public static void main(String[] args) {
         System.out.println(a);
     }
---
+++ 28-jvm/src/JVM/chapter02/src/java/lang/String.java
@@ -0,0 +1,16 @@
+package java.lang;
+
+/**
+ * @author DeltaV235
+ * @version 1.0
+ * @date 2021/2/12 14:23
+ */
+public class String {
+    static {
+        System.out.println("customer String");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+}
