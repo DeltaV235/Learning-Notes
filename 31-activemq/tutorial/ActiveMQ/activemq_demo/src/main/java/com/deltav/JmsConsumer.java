43f44bfc34dbdb4ed3159c0a32323157977c8944
DeltaV235 <wuyue96@gmail.com>
2021-02-28 17:27:54 +0800

    feat(ActiveMQ): 增加了监听器接收消息的代码实现。

--- 31-activemq/tutorial/ActiveMQ/activemq_demo/src/main/java/com/deltav/JmsConsumer.java
+++ 31-activemq/tutorial/ActiveMQ/activemq_demo/src/main/java/com/deltav/JmsConsumer.java
@@ -3,6 +3,7 @@ package com.deltav;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * @author DeltaV235
@@ -13,7 +14,7 @@ public class JmsConsumer {
     private static final String ACTIVE_MQ_URL = "tcp://192.168.2.200:61616";
     private static final String QUEUE_NAME = "queue01";
 
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, IOException {
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVE_MQ_URL);
 
         Connection connection = activeMQConnectionFactory.createConnection();
@@ -24,15 +25,28 @@ public class JmsConsumer {
         Queue queue = session.createQueue(QUEUE_NAME);
         MessageConsumer consumer = session.createConsumer(queue);
 
-        while (true) {
-            TextMessage message = (TextMessage) consumer.receive();
-            if (null != message) {
-                System.out.println(message.getText());
-            } else {
-                break;
-            }
-        }
+//        while (true) {
+//            TextMessage message = (TextMessage) consumer.receive(6000L);
+//            if (null != message) {
+//                System.out.println(message.getText());
+//            } else {
+//                break;
+//            }
+//        }
 
+        // 监听器方法
+        consumer.setMessageListener(message -> {
+            if (null != message && message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println(textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        System.in.read();
         consumer.close();
         session.close();
         connection.close();
