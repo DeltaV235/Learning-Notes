2ff83f2bb64cda96e76e63d3936d4534f9c01c85
DeltaV235 <wuyue96@gmail.com>
2021-04-10 19:59:36 +0800

    feat(jvm): Add OperandStack Demo code.

---
+++ 28-jvm/src/JVM/chapter04/src/com/deltav/OperandStackDemo.java
@@ -0,0 +1,17 @@
+package com.deltav;
+
+/**
+ * 操作数栈 demo
+ *
+ * @author DeltaV235
+ * @version 1.0
+ * @date 2021/4/8 23:19
+ */
+public class OperandStackDemo {
+    public void test() {
+        byte i = 1;
+        int j = 3;
+
+        int k = i + j;
+    }
+}
