daa148af42984447eedccc73f105126236cdd155
DeltaV235 <wuyue96@gmail.com>
2021-09-12 17:30:41 +0800

    feat(SpringCloud): create submodule for payment service
    - Create spring boot application start class
    - Add application.yml of dataSource/server port/mybatis config
    - Update default pom.xml of payment service
    
    #37

---
+++ 26-spring-cloud/source/springcloud2021/cloud-provider-payment8001/src/main/java/com/deltav/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.deltav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DeltaV235
+ * @version 1.0
+ * @date 2021/9/12 17:24
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
