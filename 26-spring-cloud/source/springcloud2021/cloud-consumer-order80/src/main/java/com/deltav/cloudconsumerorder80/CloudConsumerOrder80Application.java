b83cca42506fee8640a70b6b4482f73a8fec8b25
DeltaV235 <wuyue96@gmail.com>
2021-09-25 19:48:24 +0800

    feat(SpringCloud): init order consumer module
    #37

---
+++ 26-spring-cloud/source/springcloud2021/cloud-consumer-order80/src/main/java/com/deltav/cloudconsumerorder80/CloudConsumerOrder80Application.java
@@ -0,0 +1,16 @@
+package com.deltav.cloudconsumerorder80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DeltaV
+ */
+@SpringBootApplication
+public class CloudConsumerOrder80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerOrder80Application.class, args);
+    }
+
+}
---
+++ 26-spring-cloud/source/springcloud2021/cloud-consumer-order80/src/test/java/com/deltav/cloudconsumerorder80/CloudConsumerOrder80ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deltav.cloudconsumerorder80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConsumerOrder80ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
