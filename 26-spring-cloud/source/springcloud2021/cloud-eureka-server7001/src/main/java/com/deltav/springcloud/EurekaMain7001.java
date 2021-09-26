656d20db86db4441af9fab73daa4221aaf246b73
DeltaV235 <wuyue96@gmail.com>
2021-09-26 18:56:17 +0800

    feat(SpringCloud): init Eureka server
    - Create a new module for Eureka Server
    
    #37

---
+++ 26-spring-cloud/source/springcloud2021/cloud-eureka-server7001/src/main/java/com/deltav/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.deltav.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author DeltaV235
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
