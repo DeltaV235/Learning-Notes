0ea63efb4ea2f77e2d9ea2c7cca82bdc326307a9
DeltaV235 <wuyue96@gmail.com>
2021-09-27 00:07:56 +0800

    feat(SpringCloud): register two service to Eureka Server
    - Register Order and Payment services to Eureka Server
    
    #37

--- 26-spring-cloud/source/springcloud2021/cloud-consumer-order80/src/main/java/com/deltav/cloudconsumerorder80/OrderMain80.java
+++ 26-spring-cloud/source/springcloud2021/cloud-consumer-order80/src/main/java/com/deltav/cloudconsumerorder80/OrderMain80.java
@@ -2,11 +2,13 @@ package com.deltav.cloudconsumerorder80;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author DeltaV
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- 26-spring-cloud/source/springcloud2021/cloud-provider-payment8001/src/main/java/com/deltav/cloudproviderpayment8001/PaymentMain8001.java
+++ 26-spring-cloud/source/springcloud2021/cloud-provider-payment8001/src/main/java/com/deltav/cloudproviderpayment8001/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.deltav.cloudproviderpayment8001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author DeltaV235
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/9/12 17:24
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
