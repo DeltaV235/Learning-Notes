0dd596fa5d205cf7997e98060256f9f8734a0b1b
DeltaV235 <wuyue96@gmail.com>
2021-12-25 13:57:08 +0800

    feat(JVM-GC): add byte code explain Excel file
    #36

---
+++ 28-jvm/src/JVM/chapter13-ByteCode/src/com/deltav/Demo.java
@@ -0,0 +1,17 @@
+package com.deltav;
+
+/**
+ * byte code demo
+ *
+ * @author DeltaV235
+ * @version 1.0
+ */
+public class Demo {
+    private int num = 1;
+
+
+    public int add() {
+        num = num + 2;
+        return num;
+    }
+}
