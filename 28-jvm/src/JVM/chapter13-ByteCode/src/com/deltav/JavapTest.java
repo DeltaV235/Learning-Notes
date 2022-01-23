b4b8badd273467ade694cf4a3621df3f40fa0fcb
DeltaV235 <wuyue96@gmail.com>
2022-01-24 00:50:33 +0800

    feat(JVM-GC): add javap command test code and parse information
    #36

---
+++ 28-jvm/src/JVM/chapter13-ByteCode/src/com/deltav/JavapTest.java
@@ -0,0 +1,24 @@
+package com.deltav;
+
+/**
+ * file to test javac command
+ *
+ * @author DeltaV235
+ * @version 1.0
+ */
+public class JavapTest {
+    static Integer i = 2;
+    private final String str = "test";
+    public double decimal;
+
+    public JavapTest() {
+    }
+
+    public short add() {
+        return 3;
+    }
+
+    final void calc(String str) {
+        System.out.println("str = " + str);
+    }
+}
