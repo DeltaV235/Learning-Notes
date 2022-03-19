67944234264858d0f355709542d7e66b8413bd6e
DeltaV235 <wuyue96@gmail.com>
2022-03-19 15:05:13 +0800

    docs(JVM-ClassLoader): add classloader related notes
    - Add LinkingTest demo code.
    
    #121

---
+++ 28-jvm/src/JVM/chapter14-classloader/src/com/deltav/LinkingTest.java
@@ -0,0 +1,19 @@
+package com.deltav;
+
+/**
+ * 类加载阶段 2：链接阶段
+ * <p>
+ * 基本数据类型：非 final 修饰的变量，在准备环节进行默认初始化赋值（0 or false or null）。
+ * final 修饰后，在准备环节直接进行显示赋值。
+ * <p>
+ * 如果使用字面量的方式定义一个字符串的常量的话，也在准备阶段直接进行显示赋值。
+ *
+ * @author DeltaV235
+ * @version 1.0
+ */
+public class LinkingTest {
+    private static final int num = 1;
+    private static final String constStr = "CONST";
+    private static final String constStr1 = new String("CONST");
+    private static long id;
+}
