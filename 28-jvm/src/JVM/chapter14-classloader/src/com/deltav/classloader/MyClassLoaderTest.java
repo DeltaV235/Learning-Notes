d02e9050b7f0370f688a352a09be0eb915b4b989
DeltaV235 <wuyue96@gmail.com>
2022-03-27 17:05:26 +0800

    style(JVM-ClassLoader): add comment
    #121

--- 28-jvm/src/JVM/chapter14-classloader/src/com/deltav/classloader/MyClassLoaderTest.java
+++ 28-jvm/src/JVM/chapter14-classloader/src/com/deltav/classloader/MyClassLoaderTest.java
@@ -8,6 +8,7 @@ public class MyClassLoaderTest {
     public static void main(String[] args) {
         MyClassLoader myClassLoader = new MyClassLoader("chapter14-classloader\\src\\com\\deltav\\classloader");
         try {
+            // 由于在此没有使用全类名，所以自定义类加载器的父类认为该类未被加载，且该类的路径无法被父类加载器加载
             Class<?> user = myClassLoader.loadClass("User");
             System.out.println(user.getClassLoader().getClass().getName());
             System.out.println(user.getClassLoader().getParent().getClass().getName());
