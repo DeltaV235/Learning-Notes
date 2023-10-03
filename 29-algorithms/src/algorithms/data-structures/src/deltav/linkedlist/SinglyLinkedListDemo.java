7e794c8af72bc809aef744dd746545be21b0254f
DeltaV235 <wuyue96@gmail.com>
2023-10-03 14:25:33 +0800

    feat(linkedList): add method to sort insert sequence

--- 29-algorithms/src/algorithms/data-structures/src/deltav/linkedlist/SinglyLinkedListDemo.java
+++ 29-algorithms/src/algorithms/data-structures/src/deltav/linkedlist/SinglyLinkedListDemo.java
@@ -3,14 +3,19 @@ package deltav.linkedlist;
 public class SinglyLinkedListDemo {
     public static void main(String[] args) {
         SinglyLinkedList linkedList = new SinglyLinkedList();
-        HeroNode hero1 = new HeroNode(1, "test1", "test2");
+        HeroNode hero1 = new HeroNode(1, "test1", "test1");
         HeroNode hero2 = new HeroNode(2, "test2", "test2");
         HeroNode hero3 = new HeroNode(3, "test3", "test3");
         HeroNode hero4 = new HeroNode(4, "test4", "test4");
-        linkedList.add(hero1);
-        linkedList.add(hero2);
-        linkedList.add(hero3);
-        linkedList.add(hero4);
+//        linkedList.add(hero1);
+//        linkedList.add(hero4);
+//        linkedList.add(hero2);
+//        linkedList.add(hero3);
+
+        linkedList.addByOrder(hero4);
+        linkedList.addByOrder(hero2);
+        linkedList.addByOrder(hero3);
+        linkedList.addByOrder(hero1);
 
         linkedList.showList();
     }
@@ -25,6 +30,23 @@ class SinglyLinkedList {
             temp = temp.next;
         }
         temp.next = newHeroNode;
+    }
+
+    public void addByOrder(HeroNode heroNode) {
+        HeroNode temp = head;
+        while (true) {
+            if (null == temp.next) {
+                break;
+            } else if (temp.next.no > heroNode.no) {
+                break;
+            } else if (temp.next.no.equals(heroNode.no)) {
+                System.out.printf("new hero's no is duplicate with exist one, heroNo = %d\n", heroNode.no);
+                return;
+            }
+            temp = temp.next;
+        }
+        heroNode.next = temp.next;
+        temp.next = heroNode;
     }
 
     public void showList() {
