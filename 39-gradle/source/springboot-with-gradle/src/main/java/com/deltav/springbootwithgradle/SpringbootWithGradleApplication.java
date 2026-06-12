9aa97353a4e6148d353171e5d0c2c6d863d7cb28
DeltaV235 <wuyue96@gmail.com>
2026-06-12 13:39:42 +0800

    feat(gradle): add Spring Boot Gradle example
    - Add Spring Boot 3.5.3 Gradle project scaffold under 39-gradle
    - Include Gradle wrapper, build configuration, application class, and test skeleton

---
+++ 39-gradle/source/springboot-with-gradle/src/main/java/com/deltav/springbootwithgradle/SpringbootWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.deltav.springbootwithgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithGradleApplication.class, args);
+    }
+
+}
---
+++ 39-gradle/source/springboot-with-gradle/src/test/java/com/deltav/springbootwithgradle/SpringbootWithGradleApplicationTests.java
@@ -0,0 +1,20 @@
+package com.deltav.springbootwithgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@SpringBootTest
+class SpringbootWithGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Map<Character, Integer> map= new HashMap<>();
+        map.get
+    }
+
+}
